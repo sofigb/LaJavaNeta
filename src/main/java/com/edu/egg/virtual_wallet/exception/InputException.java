356d2174cff5c60ff9029cd98b2034da25addd0e
Agustin Riveros <garciariverosagustinignacio@gmail.com>
2021-12-10 19:07:17 -0300

    me olvide algo xd

--- src/main/java/com/edu/egg/virtual_wallet/exception/InputException.java
+++ src/main/java/com/edu/egg/virtual_wallet/exception/InputException.java
@@ -20,7 +20,7 @@ public class InputException extends Exception {
 
     public static InputException NotEdited(String entity){return new InputException( "No se ha podido  editar "+entity);}
 
-    public static InputException UsedEmail(String email){return new InputException("El Email " + email +  " ya fue utilizado");}
+    //public static InputException UsedEmail(String email){return new InputException("El Email " + email +  " ya fue utilizado");}
 
     public static InputException RepeatedData(String entity){return new InputException(entity + " ya fue utilizado");}
 
